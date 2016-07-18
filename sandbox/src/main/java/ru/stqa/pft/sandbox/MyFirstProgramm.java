07c65a82242944471408e8bf2f015c5b90ef984d
EShkuratova <shkuratova.kate@gmail.com>
2016-07-18 14:28:51 +0300

    Изменен репозитрий в IDEA

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -2,9 +2,9 @@ package ru.stqa.pft.sandbox;
 
 class MyFirstProgramm {
 
-public static void main(String[] args){
+    public static void main(String[] args){
 
-System.out.println("Hello,world!");
+        System.out.println("Hello,world!");
 
 }
 
