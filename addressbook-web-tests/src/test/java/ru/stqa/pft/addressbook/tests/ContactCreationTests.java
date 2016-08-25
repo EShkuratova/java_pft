3c5b54bac6342ab0c309c084f3812701709450fe
EShkuratova <shkuratova.kate@gmail.com>
2016-08-25 20:05:23 +0300

    Парсинг параметров запуска. Параметризация тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -16,14 +18,24 @@ public class ContactCreationTests extends TestBase {
         app.goTo().homePage();
         Contacts before = app.contact().all();
         app.goTo().newContactPage();
+        File photo = new File("src/test/resources/StanWithoutHat2.jpg");
         ContactData contact1 = new ContactData().withFirstname("ttttt").withLastname("user2").withNickname("user2")
                 .withCompany("mts").withAddress("Санкт-Петербург, Учебный переулок").withMobilePhone( "+79111111111")
-                .withWorkPhone("+79112222222").withEmail("user1@gmail.com").withGroup("test1");
+                .withWorkPhone("+79112222222").withEmail("user1@gmail.com").withPhoto(photo);
         app.contact().create(contact1);
         assertThat(app.contact().count(), equalTo(before.size()+1));
         Contacts after = app.contact().all();
         assertThat(after, equalTo(before.withAdded(contact1.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
 
+    }
+
+    @Test
+
+    public void testCurrentDir(){
+        File currentDir = new File("."); //точка это текущая директория
+        File photo = new File("src/test/resources/StanWithoutHat2.jpg");
+        System.out.println(photo.getAbsolutePath());
+        System.out.println(photo.exists());
     }
 
 
